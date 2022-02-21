854620039fdfc07036fd87917065f17d3f0369d1
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-21 17:21:41 -0300

    Add project files.

---
+++ Atividade7_AcademiadoProgramador.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Atividade7_AcademiadoProgramador.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void maiorTestagem(ref int maior, ref int[] numeros)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (numeros[i] > maior)
+                   {
+                       maior = numeros[i];
+                   }
+                }
+            }
+
+
+            int[] numeros = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite o numero " + i + ": ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int maior = 0;
+            maiorTestagem(ref maior, ref numeros);
+            Console.Write("O maior numero é o: " + maior);
+
+        }
+    }
+}
