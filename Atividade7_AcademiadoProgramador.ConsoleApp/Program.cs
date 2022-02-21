681ff9bdc7b185fa15d7178463a5f11482757131
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-21 17:45:45 -0300

    média aritmética funcionando

--- Atividade7_AcademiadoProgramador.ConsoleApp/Program.cs
+++ Atividade7_AcademiadoProgramador.ConsoleApp/Program.cs
@@ -15,10 +15,37 @@ namespace Atividade7_AcademiadoProgramador.ConsoleApp
                        maior = numeros[i];
                    }
                 }
+            }   
+
+            //static void menorTestagem(out int menor, out int[] numeros)
+            //{
+            //    menor = 9999999;
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        if (numeros[i] < menor)
+            //        {
+            //            menor = numeros[i];
+            //        }
+            //    }
+            //}
+
+            static int media(int[] numeros)
+            {
+                //int soma = 0;
+                int cont = 0;
+                int media;
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    cont = cont + numeros[i];
+                }
+
+                media = cont / numeros.Length;
+                return media;
             }
 
-
             int[] numeros = new int[10];
+            int maior = 0;
+            int menor;
 
             for (int i = 0; i < 10; i++)
             {
@@ -26,9 +53,18 @@ namespace Atividade7_AcademiadoProgramador.ConsoleApp
                 numeros[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            int maior = 0;
             maiorTestagem(ref maior, ref numeros);
+            Console.WriteLine();
             Console.Write("O maior numero é o: " + maior);
+
+            //menorTestagem(out menor, out numeros);
+            //Console.WriteLine();
+            //Console.Write("O maior numero é o: " + menor);
+
+            
+            Console.WriteLine();
+            Console.Write("A média aritmética é: " + media(numeros));
+            
 
         }
     }
